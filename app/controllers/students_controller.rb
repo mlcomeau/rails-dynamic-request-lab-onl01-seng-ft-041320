70e910fcaed4cb060c6e3b599ab2e9ad97c7cbd2
Megan Comeau <mlcomeau99@gmail.com>
2020-06-09 21:40:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,8 +6,5 @@ class StudentsController < ApplicationController
   def show 
     @student = Student.find(params[:id])
   end 
-<<<<<<< HEAD
 
-=======
->>>>>>> 7aeafe98d17caed13fbc1666238a5b04205310cb
 end
